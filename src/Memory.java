17af57fc1df2cd2efd0891dc87522b22e64e0776
Blessing Osakue <iyoworks@gmail.com>
2013-10-09 22:36:30 -0500

    memory is accessed through a process
    Signed-off-by: Blessing Osakue <iyoworks@gmail.com>

--- src/Memory.java
+++ src/Memory.java
@@ -1,57 +1,51 @@
+
 /**
- * It will consist of 2000 integer entries, 0-999 for the program, 1000-1999 for the interrupt handler.
- * The program cannot access addresses above 999 (exits with error message).
- * Memory will initialize itself by reading a file for the program and one for the interrupt handler (if used).
- * Each line in the file will hold one integer instruction or operand, optionally followed by a comment.
- * It will support two operations:
- *     read(address) returns the value at the address
- *     write(address, data) writes the data to the address
+ * It will consist of 2000 integer entries, 0-999 for the program, 1000-1999 for
+ * the interrupt handler. The program cannot access addresses above 999 (exits
+ * with error message). Memory will initialize itself by reading a file for the
+ * program and one for the interrupt handler (if used). Each line in the file
+ * will hold one integer instruction or operand, optionally followed by a
+ * comment. It will support two operations: read(address) returns the value at
+ * the address write(address, data) writes the data to the address
+ *
  * @author Blessing Osakue
  */
-
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Memory {
-    
-    private int[] data;
 
-   // public static void main(String args[]) {
-   //     Memory mem = new Memory();
-       
-   //     Scanner sc = new Scanner(System.in);
-   //     int address = 0;
-   //     int data = 0;
-   //     boolean isWrite = false;
-   //     if (sc.hasNextInt()) {
-   //         address = sc.nextInt();
-   //     }
-   //     else {
-   //         System.out.println("Expected Integer");
-   //         System.exit(-1);
-   //     }
-       
-   //     if (sc.hasNextInt()) {
-   //         isWrite = true;
-   //         data = sc.nextInt();
-   //     }
-       
-   //     if (isWrite){
-   //         mem.write(address, data);
-   //     }
-   //     else{
-   //          System.out.println(mem.read(address));
-   //     }
-   // }
+    private static int[] data = new int[2000];
 
-    public Memory() {
-        this.data = new int[2000];
+    public static void main(String args[]) {
+        Memory mem = new Memory();
+
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNextLine()) {
+            String str = sc.nextLine();
+//            System.out.println("LN - " + str);
+            int address;
+            if (str.matches("(\\d+) (\\d+).*")) {
+                Scanner strSc = new Scanner(str);
+                address = Integer.valueOf(strSc.next());
+                String dataIn = strSc.next();
+                mem.write(address, dataIn);
+//                System.out.println("WRITE[" + address + "] " + dataIn);
+            }else{
+                address = Integer.valueOf(str);
+                System.out.println("READ[" + address + "] " + mem.read(address));
+            }
+        }
+
     }
 
     public int read(int address) {
-        return this.data[address];
+        return data[address];
     }
 
-    public void write(int address, int data) {
-        this.data[address] = data;
+    public void write(int address, String dataIn) {
+        dataIn = dataIn.replaceFirst(".*?(\\d+).*", "$1");
+        data[address] = Integer.valueOf(dataIn);
     }
 }
--- src/ProcessesIPC.java
+++ src/ProcessesIPC.java
@@ -3,8 +3,9 @@
  * and open the template in the editor.
  */
 
-
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
@@ -16,44 +17,108 @@ import java.util.Scanner;
  */
 public class ProcessesIPC {
 
+    private boolean debugMode = true;
+    private File program;
+    private Runtime rt;
+    private Process mem;
+    private OutputStream os;
+    private PrintWriter pw;
+    private CPU cpu;
+
     public static void main(String args[]) {
         try {
-            File program = validateArguments(args);
-            
-            Runtime rt = Runtime.getRuntime();
-          
-            Scanner scan = new Scanner(program);
-            
-            while(scan.hasNextInt()){
-                System.out.println(scan.nextInt());
-            }
-            
-           System.exit(0);
+            ProcessesIPC bridge = new ProcessesIPC();
+            int exitVal = bridge.run(args);
+            System.out.println("Process exited: " + exitVal);
+            System.exit(0);
 
         } catch (Throwable t) {
             t.printStackTrace();
         }
     }
 
+    public int run(String args[]) throws IOException, InterruptedException {
+
+        validateArguments(args);
+
+        rt = Runtime.getRuntime();
+
+        mem = rt.exec("java Memory");
+
+        os = mem.getOutputStream();
+
+        pw = new PrintWriter(os);
+
+        cpu = new CPU();
+
+        initMemory();
+
+        //read from mem
+        pw.format("%d\n", 3);
+        pw.flush();
+
+        pw.format("%d\n", 8);
+        pw.flush();
+
+        //finished writing and reading
+        pw.close();
+
+        InputStream is = mem.getInputStream();
+        int x = 0;
+        while ((x = is.read()) != -1) {
+            System.out.print((char) x);
+        }
+
+        mem.waitFor();
+
+        return mem.exitValue();
+
+    }
+
+    protected void initMemory() throws FileNotFoundException {
+        //initialize memory with the program
+        Scanner scan = new Scanner(program);
+        debug("READ PROGRAM");
+        String ln;
+        int i = 0;
+        while (scan.hasNext()) {
+            ln = scan.nextLine();
+            //write to mem
+            pw.format("%d %s\n", i, ln);
+            pw.flush();
+            debug("[" + i + "] - " + ln);
+            i++;
+        }
+    }
+
     /**
-     * Validate arguments passed to the program
+     * Validate arguments passed to the program and
+     * creates program object
      *
      * @param args
-     * @return File program file
      */
-    public static File validateArguments(String[] args) {
-//        for (String s : args) {
-//            System.out.println("arg -> " + s);
-//        }
+    protected void validateArguments(String[] args) {
+
+        if (debugMode) {
+            debug("ARGUMENTS");
+            for (String s : args) {
+                debug("\t" + s);
+            }
+        }
+
         if (args.length < 1) {
             error("IO [path to program]");
         }
 
-        File file = new File(args[0]);
-        if (!file.exists()) {
+        program = new File(args[0]);
+        if (!program.exists()) {
             error("Program does not exist.");
         }
-        return file;
+
+        if (args.length == 2) {
+            debugMode = args[1].equals("-debug");
+        }
+        debug("PROGRAM VALIDATED");
     }
 
     /**
@@ -61,8 +126,14 @@ public class ProcessesIPC {
      *
      * @param msg
      */
-    public static void error(String msg) {
+    protected void error(String msg) {
         System.out.println(msg);
-        System.exit(0);
+        System.exit(-1);
+    }
+
+    protected void debug(String msg) {
+        if (debugMode) {
+            System.out.println(msg);
+        }
     }
 }
